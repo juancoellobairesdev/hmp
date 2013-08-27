2ddbac072ebaf113d6d61be3b09703c68f0644e8
juancoellobairesdev <juan.coello@bairesdev.com>
2013-08-27 10:10:29 -0300

    Changing pcs

--- application/controllers/tracking.php
+++ application/controllers/tracking.php
@@ -6,7 +6,7 @@ class Tracking extends MY_Controller {
         $this->load->model('resource_model');
     }
 
-    public function poster(){
+    public function enter(){
         $user = $this->session->userdata('user');
         $teacher = $this->session->userdata('teacher');
         $teachers = array();
@@ -16,20 +16,27 @@ class Tracking extends MY_Controller {
 
         $school = $this->school_model->get($teacher->schoolId);
         $resources = $this->resource_model->get_for_poster($teacher->gradeLevel, $school->startingSchoolYear);
+        $grades = $teacher->gradeLevel == Teacher_model::$GRADE_LEVEL_S? $this->teacher_model->grades(): array($teacher->gradeLevel);
 
         $params['user'] = $user;
         $params['teacher'] = $teacher;
         $params['teachers'] = $teachers;
         $params['resources'] = $resources;
         $params['school'] = $school;
+        $params['grades'] = $grades;
         $params['months'] = Misc_helper::str_months();
 
-        $this->template('tracking/poster', $params);
+        $this->template('tracking/enter', $params);
     }
 
     public function submit_poster(){
         $resourceIds = $this->input->post('resourceIds');
         echo json_encode($resourceIds);
+    }
+
+    public function get_resources(){
+        $selected_grade = $this->input->post('selected_grade');
+        
     }
 }
 
